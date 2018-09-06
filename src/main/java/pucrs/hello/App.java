548d9c83b9c890a0afc653fb3663c9769036372f
NicolasOPinzon <nicolaspinzon34663904@gmail.com>
2018-09-06 16:29:00 -0300

    #1

---
+++ src/main/java/pucrs/hello/App.java
@@ -0,0 +1,16 @@
+package pucrs.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( getSaudacao());
+    }
+    public static String getSaudacao() {
+    	return "Olá, mundo!";
+    }
+}
